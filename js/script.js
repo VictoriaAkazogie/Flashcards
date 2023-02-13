b0aa4848e4bfba77733baf978845b86f983d94ed
Akhazogie Victoria <akhazogievictoria2@gmail.com>
2023-02-13 20:03:11 +0100

    add changes

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  $(".first").click(function(){
+    $("#java").fadeToggle(1000)
+  });
+  $(".left").click(function(){
+    $("#def").fadeToggle(1000)
+  });
+  $(".right").click(function(){
+    $("#pri").fadeToggle(1000)
+  });
+  $(".num").click(function(){
+    $("#numdef").fadeToggle(1000)
+  });
+  $(".string").click(function(){
+    $("#stridef").fadeToggle(1000)
+  });
+  $(".boo").click(function(){
+    $("#boodef").fadeToggle(1000)
+  });
+  $(".null").click(function(){
+    $("#nulldef").fadeToggle(1000)
+  });
+  $(".und").click(function(){
+    $("#unddef").fadeToggle(1000)
+  });
+  $(".sym").click(function(){
+    $("#symdef").fadeToggle(1000)
+  });
+})
